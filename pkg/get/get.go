b7213599491686891190188d45388aec5c953385
chengyumeng <792400644@qq.com>
2018-10-30 15:40:56 +0800

    实现 get pod 类型的资源

--- pkg/get/get.go
+++ pkg/get/get.go
@@ -12,6 +12,8 @@ import (
 	"github.com/olekukonko/tablewriter"
 )
 
+const pageSize int = 1024*1024 // 单页显示，不分页
+
 type GetProxy struct {
 	Option Option
 }
@@ -28,6 +30,16 @@ func (g *GetProxy) Get() {
 		g.getNamespace()
 	case AppType:
 		g.getApp()
+	case DeploymentType:
+	case StatefulsetType:
+	case DaemonsetType:
+	case CronjobType:
+		g.GetPod(g.Option.Resource)
+	case PodType:
+		g.GetPod(DeploymentType)
+		g.GetPod(StatefulsetType)
+		g.GetPod(DaemonsetType)
+		g.GetPod(CronjobType)
 	default:
 		fmt.Printf("Hello world!")
 	}
@@ -105,7 +117,7 @@ func (g *GetProxy) getApp() {
 }
 
 func (g *GetProxy) getAppBody(nsId int64) *AppBody {
-	url := fmt.Sprintf("%s/%s/%d/%s?pageSize=10241024", config.BaseURL, "api/v1/namespaces",nsId,"apps")
+	url := fmt.Sprintf("%s/%s/%d/%s?pageSize=%d", config.BaseURL, "api/v1/namespaces",nsId,"apps",pageSize)
 	req, _ := http.NewRequest(http.MethodGet, url, nil)
 	req.Header.Set("Authorization", "Bearer "+config.GlobalOption.Token)
 	res, _ := http.DefaultClient.Do(req)
@@ -119,6 +131,67 @@ func (g *GetProxy) getAppBody(nsId int64) *AppBody {
 		return nil
 	}
 	data := new(AppBody)
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
+
+func (g *GetProxy) GetPod(podType string) {
+	nsIds := []int64{}
+	ns := g.getNamespaceBody()
+	if g.Option.Namespace != "" {
+		for _, n := range ns.Data.Namespaces {
+			if n.Name == g.Option.Namespace {
+				nsIds = append(nsIds, n.Id)
+			}
+		}
+		if len(nsIds) == 0 {
+			panic("ERROR")
+		}
+	} else {
+		for _, n := range ns.Data.Namespaces {
+			nsIds = append(nsIds, n.Id)
+		}
+	}
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Id", "Name","Type","APP", "Namespace", "User", "CreateTime"})
+	exist := false
+	for _,nsId := range nsIds {
+		if app := g.getAppBody(nsId); app != nil {
+			for _,a := range app.Data.Apps {
+				if g.Option.App == "" || g.Option.App == a.Name {
+					data := g.getPodBody(a.Id,podType)
+					for _,pod := range data.Data.Pods {
+						exist = true
+						table.Append([]string{strconv.Itoa(int(pod.Id)), pod.Name,podType, pod.App.Name,pod.App.NSMetaData.Name, pod.User, pod.CreateTime.String()})
+					}
+				}
+			}
+		}
+
+	}
+	if exist {
+		table.Render()
+	}
+}
+
+func (g *GetProxy) getPodBody(appId int64,podType string) *PodBody {
+	url := fmt.Sprintf("%s/%s/%d/%ss?pageSize=%d", config.BaseURL, "api/v1/apps",appId,podType,pageSize)
+	req, _ := http.NewRequest(http.MethodGet, url, nil)
+	req.Header.Set("Authorization", "Bearer "+config.GlobalOption.Token)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	if res.StatusCode != http.StatusOK {
+		fmt.Println(string(body))
+		return nil
+	}
+	data := new(PodBody)
 	err = json.Unmarshal(body, &data)
 	if err != nil {
 		panic(err)
--- pkg/get/settings.go
+++ pkg/get/settings.go
@@ -3,10 +3,14 @@ package get
 import "time"
 
 const (
-	NamespaceType  string = "namespace"
-	AppType        string = "app"
-	DeploymentType string = "deployment"
-	ServiceType    string = "service"
+	NamespaceType   string = "namespace"
+	AppType         string = "app"
+	DeploymentType  string = "deployment"
+	DaemonsetType   string = "daemonset"
+	CronjobType     string = "cronjob"
+	StatefulsetType string = "statefulset"
+	PodType         string = "pod"
+	ServiceType     string = "service"
 )
 
 type Option struct {
@@ -20,30 +24,29 @@ type NamespaceBody struct {
 		User
 		Namespaces []Namespace `json:"namespaces"`
 	} `json:"data"`
-
 }
 
 type User struct {
-	Id int64 `json:"id"`
-	Name string `json:"name"`
-	Email string `json:"email"`
+	Id      int64  `json:"id"`
+	Name    string `json:"name"`
+	Email   string `json:"email"`
 	Display string `json:"display"`
-	Admin bool `json:"admin"`
+	Admin   bool   `json:"admin"`
 }
 
 type Namespace struct {
-	Id int64 `json:"id"`
-	Name string `json:"name"`
-	Metadata string `json:"metadata"`
+	Id         int64      `json:"id"`
+	Name       string     `json:"name"`
+	Metadata   string     `json:"metadata"`
 	CreateTime *time.Time `json:"createTime"`
 	UpdateTime *time.Time `json:"updateTime"`
-	User string `json:"user"`
+	User       string     `json:"user"`
 }
 
 type Page struct {
-	PageNum int64 `json:"pageNo"`
-	PageSize int64 `json:"pageSize"`
-	TotalPage int64 `json:"totalPage"`
+	PageNum    int64 `json:"pageNo"`
+	PageSize   int64 `json:"pageSize"`
+	TotalPage  int64 `json:"totalPage"`
 	TotalCount int64 `json:"totalCount"`
 }
 
@@ -51,13 +54,31 @@ type AppBody struct {
 	Data struct {
 		Page
 		Apps []App `json:"list"`
-	}
+	} `json:"data"`
 }
 
 type App struct {
-	Id int64 `json:"id"`
-	Name string `json:"name"`
-	Namespace string `json:"namespaceName"`
-	User string `json:"user"`
+	Id         int64      `json:"id"`
+	Name       string     `json:"name"`
+	Namespace  string     `json:"namespaceName"`
+	User       string     `json:"user"`
 	CreateTime *time.Time `json:"createTime"`
+	NSMetaData Namespace `json:"namespace"`
+}
+
+type PodBody struct {
+	Data struct {
+		Page
+		Pods []Pod `json:"list"`
+	} `json:"data"`
+}
+
+type Pod struct {
+	Id         int64      `json:"id"`
+	Name       string     `json:"name"`
+	Metadata   string     `json:"metadata"`
+	CreateTime *time.Time `json:"createTime"`
+	UpdateTime *time.Time `json:"updateTime"`
+	User       string     `json:"user"`
+	App        App       `json:"app"`
 }
