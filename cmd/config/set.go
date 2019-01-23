b8241c1e5d6a34816d8897953c1fe65cc382af76
chengyumeng <792400644@qq.com>
2019-01-23 15:34:33 +0800

    Add set system in khadijah config set command

--- cmd/config/set.go
+++ cmd/config/set.go
@@ -7,6 +7,9 @@ import (
 
 var APIkey string
 
+var System config.System
+
+
 var SetCmd = &cobra.Command{
 	Use:   "set",
 	Short: "Used to set some user configurations,such as `APIKey`.",
@@ -14,9 +17,19 @@ var SetCmd = &cobra.Command{
 		if len(APIkey) > 0 {
 			config.SetAPIKey(APIkey)
 		}
+		if len(System.BaseURL) > 0 {
+			config.GlobalOption.System.BaseURL = System.BaseURL
+		}
+
+		if len(System.WebsocketURL) > 0 {
+			config.GlobalOption.System.WebsocketURL = System.WebsocketURL
+		}
+		config.Save()
 	},
 }
 
 func init() {
 	SetCmd.Flags().StringVarP(&APIkey, "apikey", "a", "", "In Wayne, APIKEY is primarily used for calls to OpenAPI.")
+	SetCmd.Flags().StringVarP(&System.WebsocketURL, "websocketurl", "w", "", "Specify Wayne's websocket API call url.")
+	SetCmd.Flags().StringVarP(&System.BaseURL, "baseurl", "b", "", "Specify Wayne's generic API call url.")
 }
