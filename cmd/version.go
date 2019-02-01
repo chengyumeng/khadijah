a9a60663c0fa1723ffaa84f67511c9e3cd91e7e8
chengyumeng <792400644@qq.com>
2019-02-01 11:55:33 +0800

    Add code picture for khadijah -v command

--- cmd/version.go
+++ cmd/version.go
@@ -16,11 +16,30 @@ var versionCmd = &cobra.Command{
 }
 
 var versionData = `khadijah: A client tool for Kubernetes via Wayne.
-  version   :  %s
-  language  :  %s
-  platform  :  %s/%s
-  CPU       :  %d
-  Goroutine :  %d
+                                                                   dddddddd
+kkkkkkkk          hhhhhhh                                          d::::::d  iiii   jjjj                  hhhhhhh
+k::::::k          h:::::h                                          d::::::d i::::i j::::j                 h:::::h
+k::::::k          h:::::h                                          d::::::d  iiii   jjjj                  h:::::h
+k::::::k          h:::::h                                          d:::::d                                h:::::h
+ k:::::k    kkkkkkkh::::h hhhhh         aaaaaaaaaaaaa      ddddddddd:::::d iiiiiiijjjjjjj  aaaaaaaaaaaaa   h::::h hhhhh
+ k:::::k   k:::::k h::::hh:::::hhh      a::::::::::::a   dd::::::::::::::d i:::::ij:::::j  a::::::::::::a  h::::hh:::::hhh
+ k:::::k  k:::::k  h::::::::::::::hh    aaaaaaaaa:::::a d::::::::::::::::d  i::::i j::::j  aaaaaaaaa:::::a h::::::::::::::hh
+ k:::::k k:::::k   h:::::::hhh::::::h            a::::ad:::::::ddddd:::::d  i::::i j::::j           a::::a h:::::::hhh::::::h
+ k::::::k:::::k    h::::::h   h::::::h    aaaaaaa:::::ad::::::d    d:::::d  i::::i j::::j    aaaaaaa:::::a h::::::h   h::::::h
+ k:::::::::::k     h:::::h     h:::::h  aa::::::::::::ad:::::d     d:::::d  i::::i j::::j  aa::::::::::::a h:::::h     h:::::h
+ k:::::::::::k     h:::::h     h:::::h a::::aaaa::::::ad:::::d     d:::::d  i::::i j::::j a::::aaaa::::::a h:::::h     h:::::h
+ k::::::k:::::k    h:::::h     h:::::ha::::a    a:::::ad:::::d     d:::::d  i::::i j::::ja::::a    a:::::a h:::::h     h:::::h
+k::::::k k:::::k   h:::::h     h:::::ha::::a    a:::::ad::::::ddddd::::::ddi::::::ij::::ja::::a    a:::::a h:::::h     h:::::h
+k::::::k  k:::::k  h:::::h     h:::::ha:::::aaaa::::::a d:::::::::::::::::di::::::ij::::ja:::::aaaa::::::a h:::::h     h:::::h
+k::::::k   k:::::k h:::::h     h:::::h a::::::::::aa:::a d:::::::::ddd::::di::::::ij::::j a::::::::::aa:::ah:::::h     h:::::h
+kkkkkkkk    kkkkkkkhhhhhhh     hhhhhhh  aaaaaaaaaa  aaaa  ddddddddd   dddddiiiiiiiij::::j  aaaaaaaaaa  aaaahhhhhhh     hhhhhhh
+                                                                                   j::::j
+                                                                         jjjj      j::::j  version   :  %s
+                                                                        j::::jj   j:::::j  language  :  %s
+                                                                        j::::::jjj::::::j  platform  :  %s/%s
+                                                                         jj::::::::::::j   CPU       :  %d
+                                                                           jjj::::::jjj    Goroutine :  %d
+                                                                              jjjjjj
 `
 
 func versionE(cmd *cobra.Command, args []string) {
