a36f450d60fc3045a553203e771f7a61e60c4d30
chengyumeng <792400644@qq.com>
2019-02-12 11:43:38 +0800

    Set golint to pkg/describe

--- pkg/describe/settings.go
+++ pkg/describe/settings.go
@@ -2,6 +2,7 @@ package describe
 
 import "github.com/chengyumeng/khadijah/pkg/utils/resource"
 
+// Option is the option of describe proxy
 type Option struct {
 	resource  string
 	Namespace string
